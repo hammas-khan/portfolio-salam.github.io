96cf2c7d21679e586712e55592a36ba7486a99b1
Hammas Khan <hammaskhattak27@gmail.com>
2021-12-09 12:14:59 +0500

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,9 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+
+burger.addEventListener('click', ()=>{
+    navbar.classList.toggle('navbar');
+})
+navbar.addEventListener('click', ()=>{
+    navbar.classList.toggle('navbar');
+})
